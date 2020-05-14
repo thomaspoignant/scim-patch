48e08fdc1f0c35c54d7abb13e75c08078b096bac
github-action-benchmark <github@users.noreply.github.com>
2020-05-14 14:45:21 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for e69be4f806a5e7008059332d136bd59a938ba77b

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1589467175990,
+  "lastUpdate": 1589467521279,
   "repoUrl": "https://github.com/thomaspoignant/scim-patch",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -38,6 +38,44 @@ window.BENCHMARK_DATA = {
             "range": "±2.03%",
             "unit": "ops/sec",
             "extra": "86 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomas.poignant@gmail.com",
+            "name": "Thomas Poignant",
+            "username": "thomaspoignant"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e69be4f806a5e7008059332d136bd59a938ba77b",
+          "message": "Adding performance check link (#26)",
+          "timestamp": "2020-05-14T16:44:44+02:00",
+          "tree_id": "8117504337e677cfd0b67bf6a53861ce7af5d091",
+          "url": "https://github.com/thomaspoignant/scim-patch/commit/e69be4f806a5e7008059332d136bd59a938ba77b"
+        },
+        "date": 1589467520702,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "Replace query",
+            "value": 324945,
+            "range": "±1.58%",
+            "unit": "ops/sec",
+            "extra": "88 samples"
+          },
+          {
+            "name": "Add query",
+            "value": 950800,
+            "range": "±3.77%",
+            "unit": "ops/sec",
+            "extra": "89 samples"
           }
         ]
       }
