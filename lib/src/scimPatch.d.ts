197644b5797c6f19f96d5e246271c880ccda4a7e
Leonardo Speranzon <leonardo.speranzon@monokee.com>
2023-10-27 16:04:07 +0200

    Revert "Build done"
    This reverts commit e8354a64a4ec66d1d7f0eceaf536377c9914f6af.

--- lib/src/scimPatch.d.ts
+++
@@ -1,6 +0,0 @@
-import { ScimError, InvalidScimPatch, InvalidScimPatchOp, NoPathInScimPatchOp, InvalidScimPatchRequest, NoTarget, RemoveValueNestedArrayNotSupported, RemoveValueNotArray, InvalidScimRemoveValue } from './errors/scimErrors';
-import { ScimPatchSchema, ScimId, ScimSchema, ScimPatchOperation, ScimPatchRemoveOperation, ScimPatchAddReplaceOperation, ScimPatch, ScimResource, ScimMeta, ScimPatchOptions } from './types/types';
-export { ScimPatchSchema, ScimId, ScimSchema, ScimPatchOperation, ScimPatchRemoveOperation, ScimPatchAddReplaceOperation, ScimPatch, ScimResource, ScimMeta, ScimError, InvalidScimPatch, InvalidScimPatchOp, NoPathInScimPatchOp, InvalidScimPatchRequest, NoTarget, RemoveValueNestedArrayNotSupported, RemoveValueNotArray, InvalidScimRemoveValue };
-export declare const PATCH_OPERATION_SCHEMA = "urn:ietf:params:scim:api:messages:2.0:PatchOp";
-export declare function patchBodyValidation(body: ScimPatch): void;
-export declare function scimPatch<T extends ScimResource>(scimResource: T, patchOperations: Array<ScimPatchOperation>, options?: ScimPatchOptions): T;
